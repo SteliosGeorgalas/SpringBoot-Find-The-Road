11437acbb778b92c3143bcdb804914a7939bf6c5
stelios <stelios_geo1@yahoo.gr>
2021-12-22 17:57:59 +0200

    first commit

---
+++ src/test/java/gr/mindthecode/findtheroad/SpringBootFindTheRoadApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.mindthecode.findtheroad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFindTheRoadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
