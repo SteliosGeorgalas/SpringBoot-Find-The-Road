81d01eb39441aea37f4f1deff83773192fc11f50
stelios <stelios_geo1@yahoo.gr>
2021-12-31 19:26:23 +0200

    Login Auth

--- src/main/java/gr/mindthecode/findtheroad/controllers/MVC/LoginController.java
+++ src/main/java/gr/mindthecode/findtheroad/controllers/MVC/LoginController.java
@@ -7,12 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController1 {
+public class LoginController {
 
     @GetMapping("/login")
     public String home() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        //If not logged in yet return
         if(authentication == null || authentication instanceof AnonymousAuthenticationToken){
             return "login";
         }
