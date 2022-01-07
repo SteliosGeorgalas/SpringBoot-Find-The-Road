50a192d1cda15d50d6150fad9fdf1acea7757311
demetrees <alekopoulosd@gmail.com>
2022-01-07 17:53:33 +0200

    --

--- FindTheRoad-API/src/main/java/gr/mindthecode/findtheroadapi/FindTheRoadApiApplication.java
+++ FindTheRoad-API/src/main/java/gr/mindthecode/findtheroadapi/FindTheRoadApiApplication.java
@@ -3,11 +3,15 @@ package gr.mindthecode.findtheroadapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @ComponentScan({"gr.mindthecode.findtheroadmiddleware"})
+@EntityScan("gr.mindthecode.findtheroadmiddleware")
+@EnableMongoRepositories("gr.mindthecode.findtheroadmiddleware.repositories")
 public class FindTheRoadApiApplication {
 
     public static void main(String[] args) {
