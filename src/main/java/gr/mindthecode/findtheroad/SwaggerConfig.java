68089d3cadaefafffec34f8e0ba991577b0af5ce
evangelos <Vangelis.Tsagkournis@outlook.com>
2022-01-08 07:00:17 +0200

    Updated SwaggerConfig.java, annotated ButtonController to not be included in documentation.

--- src/main/java/gr/mindthecode/findtheroad/SwaggerConfig.java
+++ src/main/java/gr/mindthecode/findtheroad/SwaggerConfig.java
@@ -3,7 +3,6 @@ package gr.mindthecode.findtheroad;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
@@ -12,14 +11,16 @@ import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.Collections;
 
+import static springfox.documentation.builders.PathSelectors.regex;
+
 @Configuration
-public class swaggerConfig {
+public class SwaggerConfig {
 
     @Bean
     public Docket swaggerConfiguration() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .paths(PathSelectors.ant("/api/*"))
+                .paths(regex("/api.*")) //
                 .apis(RequestHandlerSelectors.basePackage("gr.mindthecode.findtheroad"))
                 .build()
                 .apiInfo(apiInfo());
--- src/main/java/gr/mindthecode/findtheroad/controllers/MVC/ButtonController.java
+++ src/main/java/gr/mindthecode/findtheroad/controllers/MVC/ButtonController.java
@@ -5,10 +5,12 @@ import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import springfox.documentation.annotations.ApiIgnore;
 
 @Controller
 @NoArgsConstructor
 
+@ApiIgnore
 public class ButtonController {
 
     @Autowired
